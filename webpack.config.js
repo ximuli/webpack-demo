06b59238f3ec4d1e58c9de2b8b73170ad810f15b
ximuli <ximuli666@163.com>
2019-09-18 22:02:41 +0800

    解析less

--- webpack.config.js
+++ webpack.config.js
@@ -42,6 +42,14 @@ module.exports = {
           'style-loader',
           'css-loader'
         ]
+      },
+      {
+        test: /.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'less-loader'
+        ]
       }
     ]
   },
